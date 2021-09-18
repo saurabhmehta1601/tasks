81bebe737e476809ca9fc412d50ecf0397e54e74
saurabh <saurabhmehta1601@gmail.com>
2021-09-18 10:32:19 +0530

    refactor printHelp

---
+++ bin/todos.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+const printHelp = require("../lib/printHelp")
+const addTodo = require("../lib/addTodo");
+const showTodos = require("../lib/showTodos");
+const markTodoAsDone = require("../lib/markTodoAsDone");
+const deleteTodos = require("../lib/deleteTodo");
+
+if(process.argv.length <= 2 ){
+    printHelp()
+}
+else if(process.argv[2] === "add"){
+    addTodo()
+} 
+else if(process.argv[2] === "show"){
+    showTodos()
+}
+else if(process.argv[2] === "markdone"){
+    markTodoAsDone()
+}
+else if(process.argv[2] === 'delete'){
+    deleteTodos()
+}else{
+    printHelp()
+}
+
+
